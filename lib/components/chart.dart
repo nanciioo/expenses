826ff9ead54e0b45815fd27169e161b04c504817
Venancio <venancior1998@gmail.com>
2021-08-16 12:10:21 -0300

    adicionando o arquivo teste.exe.txt e uma alteração de cores na lib/components/chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 import 'chart_bar.dart';
-
+//aaaaaaa//
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransaction;
 
